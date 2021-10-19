279a637be57fffe122db38a8f504d887edb27916
julio11lp <julio11lp@hotmail.com>
2021-10-19 16:41:45 -0300

    Updates

---
+++ precache-manifest.8a6e3390d5f54eff3b733a8df8f3028f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f5a4949b9b31f26443b5a1bb48fe764",
+    "url": "/Projeto-Trybe-Wallet/index.html"
+  },
+  {
+    "revision": "d98edbaeba23a1ccdf06",
+    "url": "/Projeto-Trybe-Wallet/static/css/main.606aa9ac.chunk.css"
+  },
+  {
+    "revision": "69ada6691320debdfecc",
+    "url": "/Projeto-Trybe-Wallet/static/js/2.827ee203.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Projeto-Trybe-Wallet/static/js/2.827ee203.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d98edbaeba23a1ccdf06",
+    "url": "/Projeto-Trybe-Wallet/static/js/main.afcd310c.chunk.js"
+  },
+  {
+    "revision": "113b79bbafa054c57fe1",
+    "url": "/Projeto-Trybe-Wallet/static/js/runtime-main.d1d2a234.js"
+  }
+]);
